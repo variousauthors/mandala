fdaf97343cf8e3f08d336b3846dbcc1baee21ca7
Zeigfreid <various.authors@gmail.com>
2013-04-27 23:38:26 -0700

    [WIP] Started work on toggling details for a phrase

--- app/assets/javascripts/passages.js
+++ app/assets/javascripts/passages.js
@@ -1,11 +1,28 @@
 
 $(function() {
-  $('.passage a').on('click', function(e) {
+  $('.passage a.title').on('click', function(e) {
     e.preventDefault();
 
-    $.get($(this).attr('href'), { async: true }, function(data) {
+    $.get($(this).attr('href'), function(data) {
       $('.passage').html(data);
 
     });
+  });
+
+  $('.passage a.keyword').on('click', function(e) {
+    e.preventDefault();
+    var that = this;
+
+    if ( $(this).hasClass('clicked') ) {
+      console.log($(this).children())
+      $(this).siblings('p').remove();
+
+    } else {
+      $.get("/details" + $(this).attr('href'), function(data) {
+          $(that).parent().append(data);
+      });
+    }
+
+    $(this).toggleClass('clicked');
   });
 });
