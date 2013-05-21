5ceb121a8e911b174ca4499fbae5bddf392a47d3
Zeigfreid <various.authors@gmail.com>
2013-05-21 13:05:55 -0700

    Based on coverage reports, removed some methods

--- app/controllers/passages_controller.rb
+++ app/controllers/passages_controller.rb
@@ -14,73 +14,12 @@ class PassagesController < ApplicationController
   # GET /passages/1
   # GET /passages/1.json
   def show
-    @passage = Passage.find_by_title(URI.unescape(params[:title]))
+    @passage = Passage.find_by_title!(URI.unescape(params[:title]))
     @passage.receives_damage
 
     respond_to do |format|
       format.html { render layout: false } # show.html.erb
       format.json { render json: @passage }
-    end
-  end
-
-  # GET /passages/new
-  # GET /passages/new.json
-  def new
-    @passage = Passage.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @passage }
-    end
-  end
-
-  # GET /passages/1/edit
-  def edit
-    @passage = Passage.find(params[:id])
-  end
-
-  # POST /passages
-  # POST /passages.json
-  def create
-    @passage = Passage.new(params[:passage])
-
-    respond_to do |format|
-      if @passage.save
-        format.html { redirect_to @passage, notice: 'Passage was successfully created.' }
-        format.json { render json: @passage, status: :created, location: @passage }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @passage.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PUT /passages/1
-  # PUT /passages/1.json
-  def update
-    @passage = Passage.find(params[:id])
-
-    respond_to do |format|
-      if @passage.update_attributes(params[:passage])
-        format.html { redirect_to @passage, notice: 'Passage was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @passage.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /passages/1
-  # DELETE /passages/1.json
-  def destroy
-    puts "HELLO"
-    @passage = Passage.find(params[:id])
-    @passage.receives_damage
-
-    respond_to do |format|
-      format.html { redirect_to @passage, notice: "damage done" }
-      format.json { head :no_content }
     end
   end
 end
